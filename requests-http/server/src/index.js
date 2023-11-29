b05e5ad4427b64cf8c9a8babe159fd34208ddc18
Kammai <felipeaugustobonfimdealmeida@gmail.com>
2023-11-29 14:39:50 -0300

    Criando Upload de arquivos e um backend em node.js básico

---
+++ requests-http/server/src/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const bodyparser = require('body-parser');
+const multiPart = require('connect-multiparty');
+
+const app = express();
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: true}));
+
+const corsOptions = {
+    origin: '*',
+    optionsSucessStatus: 200,
+}
+
+app.use(cors(corsOptions))
+
+const multiPartMiddleWare = multiPart({uploadDir: '../uploads'});
+app.post('/uploads',multiPartMiddleWare, (req, res) => {
+    const files = req.files;
+    console.log(files);
+    req.json({message: files})
+})
+
+app.use((err,req, res, next) => res.json({erro: erro.message}))
+
+app.listen(8000, () => {
+    console.log('Servidor porta 8000');
+})
