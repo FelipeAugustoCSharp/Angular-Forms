a03ac7a00bf912d53b2f03770ed55c701ed69633
Kammai <felipeaugustobonfimdealmeida@gmail.com>
2023-12-01 04:22:53 -0300

    finalizacao do módulo HTTP

--- requests-http/src/app/reactive-search/lib-search/lib-search.component.ts
+++ requests-http/src/app/reactive-search/lib-search/lib-search.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Observable, debounceTime, distinctUntilChanged, filter, map, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-lib-search',
@@ -8,16 +10,59 @@ import { FormControl } from '@angular/forms';
 })
 export class LibSearchComponent implements OnInit {
   
+  queryField = new FormControl();
+  readonly SEARCH_URL = 'https://api.cdnjs.com/libraries';
+  results$!: Observable<any>;
+  total!:number;
+  readonly CAMPOS = '=name,description,version,homepage'
+  
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
 
-  ngOnInit() {    
+  ngOnInit() { 
+    this.results$ = this.queryField.valueChanges
+    .pipe(
+      map(res => res.trim()),
+      filter(value => value.length > 1),  
+      debounceTime(150),    
+      distinctUntilChanged(),
+      //tap(value => console.log(value)),
+      switchMap(value => this.http.get(this.SEARCH_URL,{
+        params: {
+          search: value,
+          fields: this.CAMPOS
+        }
+      })),
+      tap((res: any) => this.total = res.total),
+      map((res:any) =>  res.results )
+    )
   }
 
-  queryField = new FormControl()
 
   onSearch() {
-    console.log(this.queryField.value);
+    console.log(this.queryField.value);  
+    let value = this.queryField.value
+    if (value && (value = value.trim() !== '')) {
+    // this.results$ = this.http.get(`${this.SEARCH_URL}?search=angular`)
+    
+    const params_ = {
+      search: value,
+      fields: this.CAMPOS
+    };
+    let params = new HttpParams();
+    params = params.set('search', value);
+    params = params.set('fields', this.CAMPOS)
+
+    this.results$ = this.http
+    .get(`${this.SEARCH_URL}`, {params})  
+    .pipe(
+      tap((res:any) => {
+        this.total = res.total
+        console.log(res);        
+      }),
+      map((res:any) => res.results)  //regex to version: ^[0-9]{1}.[0-9]{1}.[0-9]{1}$
+    )
+  }
 }
 
 }
